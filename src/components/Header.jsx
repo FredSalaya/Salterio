6432fa479c35b33e83baae0be3c31c277ad098ba
Jorge Fred Alvarez Salaya <mk.fred2@gmail.com>
2025-07-26 01:27:05 -0600

    flag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header() {
             {/* logo solo visible en menú móvil */}
             <li className="mb-6 hidden max-lg:block">
               <a href="/">
-                <img src="/logos/logo_salterio_color.svg" alt="logo" className="w-36" />
+                <img src="/logos/logo_salterio_color.svg" alt="logo" className="w-40" />
               </a>
             </li>
 
