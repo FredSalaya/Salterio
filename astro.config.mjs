95db6741aad67529471ae7dcc0f3ce67f72f23da
Jorge Fred Alvarez Salaya <mk.fred2@gmail.com>
2025-12-28 20:33:10 -0600

    feat: Add Workbox library and service worker files, and update Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       workbox: {
         maximumFileSizeToCacheInBytes: 4000000, // 4MB to be safe
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
